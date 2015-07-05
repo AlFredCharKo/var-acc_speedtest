bd6d194dc783c3ef558c8e831c89c880ce676584
Alexander Kompch <akompch@Charly.local>
2015-07-05 18:58:50 +0200

    Initial commit from local git

---
+++ Build/Intermediates/var-acc_speedtest.build/Debug/var-acc_speedtest.build/Objects-normal/x86_64/main.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/akompch/Desktop/EANMI_SoSe15/var-acc_speedtest/var-acc_speedtest/main.c
---
+++ var-acc_speedtest/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  var-acc_speedtest
+//
+//  Created by Alexander Kompch on 05.07.15.
+//  Copyright (c) 2015 Alexander Kompch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
